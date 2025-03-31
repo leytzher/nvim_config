80953c7283dc7b3387cfd8580a3754559e48a676
leytzher muro <leytzher@gmail.com>
2025-03-31 11:30:15 +0400

    Floating terminal with C-t

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,84 +1,92 @@
 --- Mason takes care of LSP
 return {
-  {
-    "williamboman/mason.nvim",
-  config = function()
-    require("mason").setup()
-  end
-  },
-  {
-    -- bridge with lspconfig
-    "williamboman/mason-lspconfig.nvim",
-    config = function()
-      require("mason-lspconfig").setup({
-        -- lsp's to be installed
-        ensure_installed = {
-          "lua_ls",
-          "arduino_language_server",
-          "asm_lsp",
-          "clangd",
-          "clojure_lsp",
-          "cssls",
-          "tailwindcss",
-          "docker_compose_language_service",
-          "dockerls",
-          "fortls",
-          "gopls",
-          "hls",
-          "html",
-          "htmx",
-          "eslint",
-          "jsonls",
-          "grammarly",
-          "jedi_language_server",
-          "svelte",
-          "zls",
---          "ts_ls",
-          "tsserver",
-          "fortls"
-          --
-        }
-    })
-    end
-  },
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		-- bridge with lspconfig
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				-- lsp's to be installed
+				ensure_installed = {
+					"lua_ls",
+					"arduino_language_server",
+					"clangd",
+					"clojure_lsp",
+					"cssls",
+					"tailwindcss",
+					"docker_compose_language_service",
+					"dockerls",
+					"fortls",
+					"gopls",
+					"hls",
+					"html",
+					"eslint",
+					"jsonls",
+					"grammarly",
+					"jedi_language_server",
+					"svelte",
+					"zls",
+					--          "ts_ls",
+					"tsserver",
+					"fortls",
+					"julials",
+					--
+				},
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      -- Establish communication between lsp and neovim using lspconfig
-      lspconfig.lua_ls.setup({capabilities = capabilities})
-      lspconfig.clangd.setup({capabilities = capabilities})
-      lspconfig.clojure_lsp.setup({capabilities = capabilities})
-      lspconfig.arduino_language_server.setup({capabilities = capabilities})
-      lspconfig.cssls.setup({capabilities = capabilities})
-      lspconfig.tailwindcss.setup({capabilities = capabilities})
-      lspconfig.docker_compose_language_service.setup({capabilities = capabilities})
-      lspconfig.dockerls.setup({capabilities = capabilities})
-      lspconfig.fortls.setup({capabilities = capabilities})
-      lspconfig.gopls.setup({capabilities = capabilities})
-      lspconfig.hls.setup({capabilities = capabilities})
-      lspconfig.html.setup({capabilities = capabilities})
-      lspconfig.htmx.setup({capabilities = capabilities})
-      lspconfig.eslint.setup({capabilities = capabilities})
-      lspconfig.jsonls.setup({capabilities = capabilities})
-      lspconfig.grammarly.setup({capabilities = capabilities})
-      lspconfig.jedi_language_server.setup({capabilities = capabilities})
-      lspconfig.svelte.setup({capabilities = capabilities})
-      lspconfig.zls.setup({capabilities = capabilities})
-      --lspconfig.ts_ls.setup({capabilities = capabilities})
-      lspconfig.tsserver.setup({capabilities = capabilities})
-      lspconfig.eslint.setup({capabilities = capabilities})
-      lspconfig.fortls.setup({capabilities = capabilities})
+			local lspconfig = require("lspconfig")
+			-- Establish communication between lsp and neovim using lspconfig
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.clangd.setup({ capabilities = capabilities })
+			lspconfig.clojure_lsp.setup({ capabilities = capabilities })
+			lspconfig.arduino_language_server.setup({ capabilities = capabilities })
+			lspconfig.cssls.setup({ capabilities = capabilities })
+			lspconfig.tailwindcss.setup({ capabilities = capabilities })
+			lspconfig.docker_compose_language_service.setup({ capabilities = capabilities })
+			lspconfig.dockerls.setup({ capabilities = capabilities })
+			lspconfig.fortls.setup({ capabilities = capabilities })
+			lspconfig.gopls.setup({ capabilities = capabilities })
+			lspconfig.hls.setup({ capabilities = capabilities })
+			lspconfig.html.setup({ capabilities = capabilities })
+			lspconfig.htmx.setup({ capabilities = capabilities })
+			lspconfig.eslint.setup({ capabilities = capabilities })
+			lspconfig.jsonls.setup({ capabilities = capabilities })
+			lspconfig.grammarly.setup({ capabilities = capabilities })
+			lspconfig.jedi_language_server.setup({ capabilities = capabilities })
+			lspconfig.svelte.setup({ capabilities = capabilities })
+			lspconfig.zls.setup({ capabilities = capabilities })
+			--lspconfig.ts_ls.setup({capabilities = capabilities})
+			lspconfig.tsserver.setup({ capabilities = capabilities })
+			lspconfig.eslint.setup({ capabilities = capabilities })
+			lspconfig.fortls.setup({ capabilities = capabilities })
+			lspconfig.mma.setup({ capabilities = capabilities })
+			lspconfig.julials.setup({
+				capabilities = capabilities,
+				on_new_config = function(new_config, _)
+					local julia = vim.fn.expand("~/.julia/environments/nvim-lspconfig/bin/julia")
+					if require("lspconfig").util.path.is_file(julia) then
+						new_config.cmd[1] = julia
+					end
+				end,
+			})
 
-      -- use Shift+K to show info of symbol under cursor in a floating window 
-      vim.keymap.set('n','K', vim.lsp.buf.hover, {})
-      -- use gd to go to definition
-      vim.keymap.set('n','gd', vim.lsp.buf.definition, {})
-      -- use <space>ca for code actions in visual and normal mode
-      vim.keymap.set({'n','v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-    end
-  },
-
+			-- use Shift+K to show info of symbol under cursor in a floating window
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			-- use gd to go to definition
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			-- use <space>ca for code actions in visual and normal mode
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,14 +1,24 @@
 return {
 	{ "akinsho/toggleterm.nvim", version = "*",
   config = function()
-  local config  = require("toggleterm")
-  config.setup({
-			open_mapping = [[C-t]],
-			hide_numbers = true,
+    local config  = require("toggleterm")
+    config.setup({
+        hide_numbers = true,
+        open_mapping = [[<C-t>]],
+        direction = "float",
+        float_opts = {
+          border = "curved",
+          width = 100, 
+          height = 30,
+          winblend = 3,
+        },
+        shade_terminals = true, 
+        start_in_insert = true, 
+        insert_mappings = true, 
+        terminal_mappings = true,
   })
-  end
-  }
+  end,
+  },
 }
-
 
 
